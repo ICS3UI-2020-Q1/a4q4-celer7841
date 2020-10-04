6650d641d1bbc13dd5d018ba0c92f4494c184785
celer7841 <celer7841@wrdsb.ca>
2020-10-04 20:09:12 +0000

    finished

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
  *
  * @author 
  */
+ import java.util.Scanner;
 public class Main {
 
   /**
@@ -10,6 +11,29 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
-  }
+    Scanner sc = new Scanner(System.in);
+    int counter = 1;
+    System.out.println("Please enter a positive integer to see the FizzBuzz numbers.");
+
+    int number = sc.nextInt();
+    while (counter <= number) {
+     if(counter %3 == 0 && counter %5 == 0) {
+        System.out.println("FizzBuzz");
+      } 
+      
+      else if(counter %3 == 0) {
+        System.out.println("Fizz");
+
+      }
+      else if(counter %5 == 0) {
+        System.out.println("Buzz");
+      }
+      else{
+        System.out.println (counter);
+        }
+      
+      counter = counter + 1;
+    }
+
+    } 
 }
